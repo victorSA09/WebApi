d6d7c7c67d0b1996946a5d3ee1dc489b1dbfaacc
victorSA09 <85522484+victorSA09@users.noreply.github.com>
2022-11-14 20:36:50 -0300

    Add files via upload

---
+++ WebApi_TESTE/Migrations/20221113180637_create.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    public partial class create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id_livro",
+                table: "tb_emprestimo",
+                newName: "Id_livro");
+
+            migrationBuilder.RenameColumn(
+                name: "id_leitor",
+                table: "tb_emprestimo",
+                newName: "Id_leitor");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "estado",
+                table: "tb_emprestimo",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "estado",
+                table: "tb_emprestimo");
+
+            migrationBuilder.RenameColumn(
+                name: "Id_livro",
+                table: "tb_emprestimo",
+                newName: "id_livro");
+
+            migrationBuilder.RenameColumn(
+                name: "Id_leitor",
+                table: "tb_emprestimo",
+                newName: "id_leitor");
+        }
+    }
+}
---
+++ WebApi_TESTE/Model/Emprestimo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Model
+{
+    public class Emprestimo
+    {
+        public int Id { get; set; }
+        public int Id_livro { get; set; }
+        public int Id_leitor { get; set; }
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime DataDevolucao { get; set; }
+        public Boolean Estado { get;  set; }
+    }
+}
